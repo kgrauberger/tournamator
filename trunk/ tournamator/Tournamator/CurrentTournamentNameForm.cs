f2ed6a25de88a29ded02ff4bdecafa319054ef6d
pacer3231 <pacer3231@d443dd3a-8578-62d4-668b-c65c787efcc5>
2010-09-09 01:28:52 +0000

    Initial check-in

---
+++ trunk/ tournamator/Tournamator/CurrentTournamentNameForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tournamator
+{
+
+    public partial class CurrentTournamentNameForm : Form
+    {
+        private string fileName;
+
+        public CurrentTournamentNameForm()
+        {
+            InitializeComponent();
+        }
+
+        public string getFileName()
+        {
+            return fileName;
+        }
+        
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            fileName = txtFileName.Text;
+
+            if (fileName == "")
+            {
+                MessageBox.Show("File name cannot be blank");
+                this.DialogResult = DialogResult.None;
+            }
+        }
+    }
+
+    
+        
+}
---
+++ trunk/ tournamator/Tournamator/SaveWarning.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tournamator
+{
+    public partial class SaveWarning : Form
+    {
+        public SaveWarning()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
